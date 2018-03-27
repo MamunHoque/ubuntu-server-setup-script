b83a076b9aa54d1e288ba4bdb3a5f94fe0e9edb8
Rasel Ahmed <rasel220790@gmail.com>
2018-03-27 20:07:51 +0600

    server-setup-script

---
+++ README.md
@@ -0,0 +1,50 @@
+# Prepare for Server Setup
+```shell
+cd ~
+git clone https://github.com/FierceBengalTiger/server-setup-script.git
+cd server-setup-script
+chmod +x install.sh
+sudo ./install.sh
+```
+- > It will create and configure your server.
+- > Just you need to add the vhost conf files as per your requirements.
+ *[We include the sample conf files]*
+- > Just you need to configure your Jenkins configuration. 
+- > Lastly it also install the mysql secure installation script so you need to configure your DB settings.
+
+# Add conf for your Domain
+```shell
+sudo cp /server-setup-script/sample_conf /etc/nginx/sites-available/example.com.conf
+sudo ln -s /etc/nginx/sites-available/example.com.conf /etc/nginx/sites-enabled/example.com.conf
+sudo vim /etc/nginx/sites-available/example.com.conf
+```
+
+# Add conf for your Domain
+```shell
+sudo cp /server-setup-script/sample_ssl_conf /etc/nginx/sites-available/example.com.conf
+sudo ln -s /etc/nginx/sites-available/example.com.conf /etc/nginx/sites-enabled/example.com.conf
+sudo vim /etc/nginx/sites-available/example.com.conf
+```
+
+- > Find `# Modify - ....` on conf file and change as per yours.
+
+# Restart Nginx server
+
+```shell
+sudo systemctl restart nginx
+```
+
+# Know Nginx status
+
+```shell
+sudo systemctl status nginx 
+```
+
+# Get First Password for Jenkins and Configure for Project
+```shell
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+```
+
+Special Thanks to [Behestee](https://github.com/behestee/awssh/).
+
+
---
+++ install.sh
@@ -0,0 +1,262 @@
+#!/usr/bin/env bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Update Package List, Update System Packages, Remove those dependencies that were installed with applications and that are no longer used by anything else on the system.
+
+apt-get -y update && apt-get -y upgrade && apt-get -y autoremove && apt-get -y autoclean
+
+# Install Some PPAs
+
+apt-get install -y software-properties-common curl
+
+apt-add-repository ppa:nginx/stable -y
+apt-add-repository ppa:ondrej/php -y
+
+curl --silent --location https://deb.nodesource.com/setup_8.x | bash -
+
+# Update Package Lists
+
+apt-get -y update
+
+# Install Some Basic Packages
+
+apt-get install -y build-essential dos2unix gcc git libmcrypt4 libpcre3-dev ntp unzip \
+make python2.7-dev python-pip re2c supervisor unattended-upgrades whois vim libnotify-bin \
+pv cifs-utils mcrypt bash-completion zsh
+
+# Install PHP Stuffs
+# Current PHP 7.2
+apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages --allow-unauthenticated \
+php7.2-cli php7.2-dev \
+php7.2-pgsql php7.2-sqlite3 php7.2-gd \
+php7.2-curl php7.2-memcached \
+php7.2-imap php7.2-mysql php7.2-mbstring \
+php7.2-xml php7.2-zip php7.2-bcmath php7.2-soap \
+php7.2-intl php7.2-readline
+
+# PHP 7.1
+apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages --allow-unauthenticated \
+php7.1-cli php7.1-dev \
+php7.1-pgsql php7.1-sqlite3 php7.1-gd \
+php7.1-curl php7.1-memcached \
+php7.1-imap php7.1-mysql php7.1-mbstring \
+php7.1-xml php7.1-zip php7.1-bcmath php7.1-soap \
+php7.1-intl php7.1-readline php-xdebug php-pear
+
+# PHP 7.0
+apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages --allow-unauthenticated \
+php7.0-cli php7.0-dev \
+php7.0-pgsql php7.0-sqlite3 php7.0-gd \
+php7.0-curl php7.0-memcached \
+php7.0-imap php7.0-mysql php7.0-mbstring \
+php7.0-xml php7.0-zip php7.0-bcmath php7.0-soap \
+php7.0-intl php7.0-readline
+
+
+update-alternatives --set php /usr/bin/php7.2
+
+# Install Composer
+
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Set Some PHP CLI Settings
+
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.2/cli/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.2/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.2/cli/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.2/cli/php.ini
+
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.1/cli/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.1/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.1/cli/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.1/cli/php.ini
+
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/cli/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/cli/php.ini
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.0/cli/php.ini
+sudo sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/cli/php.ini
+
+
+# Install Nginx & PHP-FPM
+
+apt-get install -y --allow-downgrades --allow-remove-essential --allow-change-held-packages --allow-unauthenticated \
+nginx php7.1-fpm php7.2-fpm php7.0-fpm
+
+systemctl restart nginx
+
+# Install Jenkins
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+
+apt-get update
+apt-get install -y jenkins
+
+# Start jenkins as service
+systemctl start jenkins
+systemctl enable jenkins
+
+# Setup Some PHP-FPM Options
+
+echo "xdebug.remote_enable = 1" >> /etc/php/7.2/mods-available/xdebug.ini
+echo "xdebug.remote_connect_back = 1" >> /etc/php/7.2/mods-available/xdebug.ini
+echo "xdebug.remote_port = 9000" >> /etc/php/7.2/mods-available/xdebug.ini
+echo "xdebug.max_nesting_level = 512" >> /etc/php/7.2/mods-available/xdebug.ini
+echo "opcache.revalidate_freq = 0" >> /etc/php/7.2/mods-available/opcache.ini
+
+echo "xdebug.remote_enable = 1" >> /etc/php/7.1/mods-available/xdebug.ini
+echo "xdebug.remote_connect_back = 1" >> /etc/php/7.1/mods-available/xdebug.ini
+echo "xdebug.remote_port = 9000" >> /etc/php/7.1/mods-available/xdebug.ini
+echo "xdebug.max_nesting_level = 512" >> /etc/php/7.1/mods-available/xdebug.ini
+echo "opcache.revalidate_freq = 0" >> /etc/php/7.1/mods-available/opcache.ini
+
+echo "xdebug.remote_enable = 1" >> /etc/php/7.0/mods-available/xdebug.ini
+echo "xdebug.remote_connect_back = 1" >> /etc/php/7.0/mods-available/xdebug.ini
+echo "xdebug.remote_port = 9000" >> /etc/php/7.0/mods-available/xdebug.ini
+echo "xdebug.max_nesting_level = 512" >> /etc/php/7.0/mods-available/xdebug.ini
+echo "opcache.revalidate_freq = 0" >> /etc/php/7.0/mods-available/opcache.ini
+
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.2/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.2/fpm/php.ini
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.2/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.2/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.2/fpm/php.ini
+sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.2/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.2/fpm/php.ini
+
+printf "[openssl]\n" | tee -a /etc/php/7.2/fpm/php.ini
+printf "openssl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/7.2/fpm/php.ini
+
+printf "[curl]\n" | tee -a /etc/php/7.2/fpm/php.ini
+printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/7.2/fpm/php.ini
+
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.1/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.1/fpm/php.ini
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.1/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.1/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.1/fpm/php.ini
+sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.1/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.1/fpm/php.ini
+
+printf "[openssl]\n" | tee -a /etc/php/7.1/fpm/php.ini
+printf "openssl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/7.1/fpm/php.ini
+
+printf "[curl]\n" | tee -a /etc/php/7.1/fpm/php.ini
+printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/7.1/fpm/php.ini
+
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php/7.0/fpm/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php/7.0/fpm/php.ini
+sed -i "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
+sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php/7.0/fpm/php.ini
+sed -i "s/upload_max_filesize = .*/upload_max_filesize = 100M/" /etc/php/7.0/fpm/php.ini
+sed -i "s/post_max_size = .*/post_max_size = 100M/" /etc/php/7.0/fpm/php.ini
+sed -i "s/;date.timezone.*/date.timezone = UTC/" /etc/php/7.0/fpm/php.ini
+
+printf "[curl]\n" | tee -a /etc/php/7.0/fpm/php.ini
+printf "curl.cainfo = /etc/ssl/certs/ca-certificates.crt\n" | tee -a /etc/php/7.0/fpm/php.ini
+
+# Disable XDebug On The CLI
+
+sudo phpdismod -s cli xdebug
+
+# Copy fastcgi_params to Nginx because they broke it on the PPA
+
+cat > /etc/nginx/fastcgi_params << EOF
+fastcgi_param	QUERY_STRING		\$query_string;
+fastcgi_param	REQUEST_METHOD		\$request_method;
+fastcgi_param	CONTENT_TYPE		\$content_type;
+fastcgi_param	CONTENT_LENGTH		\$content_length;
+fastcgi_param	SCRIPT_FILENAME		\$request_filename;
+fastcgi_param	SCRIPT_NAME			\$fastcgi_script_name;
+fastcgi_param	REQUEST_URI			\$request_uri;
+fastcgi_param	DOCUMENT_URI		\$document_uri;
+fastcgi_param	DOCUMENT_ROOT		\$document_root;
+fastcgi_param	SERVER_PROTOCOL		\$server_protocol;
+fastcgi_param	GATEWAY_INTERFACE	CGI/1.1;
+fastcgi_param	SERVER_SOFTWARE		nginx/\$nginx_version;
+fastcgi_param	REMOTE_ADDR			\$remote_addr;
+fastcgi_param	REMOTE_PORT			\$remote_port;
+fastcgi_param	SERVER_ADDR			\$server_addr;
+fastcgi_param	SERVER_PORT			\$server_port;
+fastcgi_param	SERVER_NAME			\$server_name;
+fastcgi_param	HTTPS				\$https if_not_empty;
+fastcgi_param	REDIRECT_STATUS		200;
+EOF
+
+# Set The Nginx & PHP-FPM User
+
+sed -i "s/user www-data;/user jenkins;/" /etc/nginx/nginx.conf
+sed -i "s/# server_names_hash_bucket_size.*/server_names_hash_bucket_size 64;/" /etc/nginx/nginx.conf
+
+sed -i "s/user = www-data/user = jenkins/" /etc/php/7.2/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = jenkins/" /etc/php/7.2/fpm/pool.d/www.conf
+
+sed -i "s/user = www-data/user = jenkins/" /etc/php/7.1/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = jenkins/" /etc/php/7.1/fpm/pool.d/www.conf
+
+sed -i "s/listen\.owner.*/listen.owner = jenkins/" /etc/php/7.1/fpm/pool.d/www.conf
+sed -i "s/listen\.group.*/listen.group = jenkins/" /etc/php/7.1/fpm/pool.d/www.conf
+sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.1/fpm/pool.d/www.conf
+
+sed -i "s/user = www-data/user = jenkins/" /etc/php/7.0/fpm/pool.d/www.conf
+sed -i "s/group = www-data/group = jenkins/" /etc/php/7.0/fpm/pool.d/www.conf
+
+sed -i "s/listen\.owner.*/listen.owner = jenkins/" /etc/php/7.0/fpm/pool.d/www.conf
+sed -i "s/listen\.group.*/listen.group = jenkins/" /etc/php/7.0/fpm/pool.d/www.conf
+sed -i "s/;listen\.mode.*/listen.mode = 0666/" /etc/php/7.0/fpm/pool.d/www.conf
+
+systemctl restart nginx
+systemctl restart php7.2-fpm
+systemctl restart php7.1-fpm
+systemctl restart php7.0-fpm
+
+systemctl enable nginx
+systemctl enable php7.2-fpm
+systemctl enable php7.1-fpm
+systemctl enable php7.0-fpm
+
+# Add jenkins User To WWW-Data
+
+usermod -a -G www-data jenkins
+id jenkins
+groups jenkins
+
+# Install MySQL
+sudo apt-get install -y mysql-server
+
+# If it’s not running, start it with this command:
+sudo systemctl start mysql
+
+#To enable MySQL to automatically start when Ubuntu is rebooted:
+sudo systemctl enable mysql
+
+# Now run the post installation security script.
+mysql_secure_installation
+
+# Configure Supervisor
+
+systemctl daemon-reload
+
+systemctl enable supervisor.service
+systemctl start supervisor
+
+# One last upgrade check
+
+apt-get -y upgrade
+
+# Clean Up
+
+apt-get -y autoremove
+apt-get -y autoclean
+chown -R jenkins:www-data /var/www
+
+# Enable Swap Memory
+
+/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+/sbin/mkswap /var/swap.1
+/sbin/swapon /var/swap.1
+
+apt-get -y update && apt-get -y upgrade && apt-get -y autoremove && apt-get -y autoclean
+
+echo "Thanks from FierceBengalTiger and Finished Server Setup...."
